ea120704dd87676cd091d98eaf27693ccc5a6e21
tomoteletoyosi <tomoteletoyosit@gmail.com>
2022-09-10 02:32:32 -0700

    myTimeProject

---
+++ src/component/Gettime/Gettime.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Gettime.css";
+
+const Gettime = () => {
+  let time = new Date().toLocaleTimeString();
+
+  const [myTime, setMyTime] = React.useState(time);
+
+  // var today = new Date();
+  // var date =
+  //   today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  // var time =
+  //   today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  // var dateTime = date + " " + time;
+  React.useEffect(
+    function () {
+      setInterval(function () {
+        getCurrentTime();
+      }, 1000);
+    },
+    [myTime]
+  );
+
+  function getCurrentTime() {
+    let updatedTime = new Date().toLocaleTimeString();
+
+    setMyTime(updatedTime);
+  }
+
+  // console.log(dateTime);
+  return (
+    <div className="container">
+      <h1>{myTime}</h1>
+      <button onClick={getCurrentTime}>GET TIME</button>
+    </div>
+  );
+};
+
+export default Gettime;
